eec1ee6e704db38e21fde4c1e0af48dcd24b36db
TpT3 <78530115+TpT3@users.noreply.github.com>
2022-08-31 14:21:06 +0300

    Online Fix
    Searching

--- Assets/Main/Scripts/UI Scripts/ServerLogic.cs
+++ Assets/Main/Scripts/UI Scripts/ServerLogic.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -39,14 +40,15 @@ public class ServerLogic : MonoBehaviour
     [SerializeField] private GameObject searchElement;
     [SerializeField] private Transform searching;
 
+    private string lastCategory = "ID";
+    private string lastSearch;
+    private int searchPage;
+
     void Start()
     {
+        rating = "1";
         currentAccount.text = "Current: " + PlayerPrefs.GetString("Login", "");
-
-        if (EditorLogic.levelName != null)
-        {
-            searchInput.text = EditorLogic.levelName;
-        }
+        searchPage = 0;
     }
 
     public void Resents()
@@ -99,19 +101,41 @@ public class ServerLogic : MonoBehaviour
         StartCoroutine(Send(url, keys, values));
     }
 
+    public void NextPage()
+    {
+        searchPage += 5;
+        Search(lastCategory);
+    }
+
+    public void PreviousPage()
+    {
+        if (searchPage >= 5)
+        {
+            searchPage -= 5;
+            Search(lastCategory);
+        }
+    }
+
     public void Search(string category)
     {
+        if (searchInput.text != lastSearch)
+        {
+            searchPage = 0;
+        }
+
+        lastCategory = category;
+        lastSearch = searchInput.text;
         url = "levelsearch.php";
 
         if (searchInput.text == "")
         {
-            keys = new string[] { "category", "rating" };
-            values = new string[] { category, rating };
+            keys = new string[] { "category", "rating", "page" };
+            values = new string[] { category, rating, Convert.ToString(searchPage) };
         }
         else
         {
-            keys = new string[] { "search", "category", "rating" };
-            values = new string[] { searchInput.text, category, rating };
+            keys = new string[] { "search", "category", "page", "rating" };
+            values = new string[] { searchInput.text, category, Convert.ToString(this.searchPage), rating };
         }
 
         StartCoroutine(Send(url, keys, values));
@@ -196,21 +220,20 @@ public class ServerLogic : MonoBehaviour
                     for (int i = 0; i < searching.childCount; i++) Destroy(searching.GetChild(i).gameObject);
                 }
 
-                SearchData searchResults = JsonUtility.FromJson<SearchData>(result);
-                for (int i = 0; i < searchResults.names.Length; i++)
+                SearchData searchData = JsonUtility.FromJson<SearchData>(result);
+                for (int k = 0; k < searchData.names.Length; k++)
                 {
 
-                    GameObject searchResult = Instantiate(
+                    Text[] info = Instantiate(
                         searchElement,
-                        new Vector3(2f, -2.2f + (searchResults.names.Length - i - 1) * 1.6f, 0),
+                        new Vector3(2f, -2.2f + (float)k * 1.6f, 0f),
                         Quaternion.Euler(Vector3.zero),
                         searching
-                    );
+                    ).GetComponentsInChildren<Text>();
 
-                    Text[] info = searchResult.GetComponentsInChildren<Text>();
-                    info[0].text = searchResults.ids[i];
-                    info[1].text = searchResults.names[i];
-                    info[2].text = searchResults.authors[i];
+                    info[0].text = searchData.ids[k];
+                    info[1].text = searchData.names[k];
+                    info[2].text = searchData.authors[k];
                 }
                 break;
         }
